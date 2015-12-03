0155fc190cabaaf649437560e5d9cb860454958f
Gyorgy Trum <gyuri.trum@gmail.com>
2015-12-03 14:15:46 +0100

    Hello Universe

--- CustomFonts/ViewController.swift
+++ CustomFonts/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        
+        
+        var helloWorld : String
         self.customFontLabel.text = "Hello World"
     }
     
